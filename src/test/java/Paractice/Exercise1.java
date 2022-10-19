4b3eb729971dcc6cda1c84064f098506881b4cd8
aymurat111 <aymurat111@gmail.com>
2022-10-19 10:42:12 +0200

    click

--- src/test/java/Paractice/Exercise1.java
+++ src/test/java/Paractice/Exercise1.java
@@ -1,8 +1,8 @@
 package Paractice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,13 +11,14 @@ import java.time.Duration;
 public class Exercise1 {
    /* BeforeClass ile driver'i olusturun ve class icinde static yapin
     Maximize edin ve 10 sn bekletin
-    http://www.google.com adresine gidin
+    https://www.google.com adresine gidin
     arama kutusuna "The Lord of the Rings" yazip, cikan sonuc sayisini yazdirin
     arama kutusuna "Brave Heart" yazip, cikan sonuc sayisini yazdirin
     arama kutusuna "Harry Potter" yazip, cikan sonuc sayisini yazdirin
     AfterClass ile kapatin     */
 
     static WebDriver driver;
+
     @BeforeClass
     public static void setUp() {
         WebDriverManager.chromedriver().setup();
@@ -26,9 +27,30 @@ public class Exercise1 {
         driver.manage().timeouts().implicitlyWait( Duration.ofSeconds( 15 ) );
 
     }
+    @Test
+    public void test01(){
+
+            }
+    @Test
+    public void test02() {
+    }
+    @Test
+    public void test03(){
+
+    }
+    @Before
+    public void beforeTest(){
+        driver.get( "https://www.google.com" );
+        driver.findElement( By.xpath( "//div[text()='Tümünü kabul et']" ) ).click();
+
+    }
+    @After
+    public void afterTest(){
+
+    }
     @AfterClass
     public static void tearDown(){
-        driver.close();
+        //driver.close();
     }
 
 }

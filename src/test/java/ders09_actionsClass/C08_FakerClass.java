d4e4cec87ed3d08860d6350880bf344430ef9900
aymurat111 <aymurat111@gmail.com>
2022-10-22 17:26:58 +0200

    monat

--- src/test/java/ders09_actionsClass/C08_FakerClass.java
+++ src/test/java/ders09_actionsClass/C08_FakerClass.java
@@ -1,6 +1,9 @@
 package ders09_actionsClass;
 import com.github.javafaker.Faker;
 import org.junit.Test;
+
+import java.util.Date;
+
 public class C08_FakerClass {
     @Test
     public void test01(){
@@ -9,5 +12,11 @@ public class C08_FakerClass {
         System.out.println(faker.name().fullName());
         System.out.println(faker.address().fullAddress());
         System.out.println(faker.internet().password());
+        System.out.println(faker.address().city().toUpperCase());
+        System.out.println(faker.date().birthday());
+        System.out.println(faker.address().streetName());
+        System.out.println(faker.address().city().toLowerCase());
+        System.out.println(faker.company().url());
+
     }
 }

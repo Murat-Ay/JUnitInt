15017e50dc3f192260ccc2539ac46a95b0143131
aymurat111 <aymurat111@gmail.com>
2022-11-01 10:30:19 +0100

    twt

--- src/test/java/paractice/TestTwitter.java
+++ src/test/java/paractice/TestTwitter.java
@@ -2,7 +2,9 @@ package paractice;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class TestTwitter extends TestBase {
@@ -15,9 +17,22 @@ public class TestTwitter extends TestBase {
     // kapatmaKutusu.click();
      driver.findElement(By.xpath("//span[text()='Kaydol']")).click();
      driver.findElement( By.xpath( "//*[text()='Telefon veya e-posta ile kaydol']" ) ).click();
-
-
-
-    }
+     WebElement isimKutusu=driver.findElement( By.xpath( "//input[@autocomplete='name']" ) );
+     Actions actions=new Actions( driver );
+     actions.click(isimKutusu)
+             .sendKeys( "Bora" )
+             .sendKeys( Keys.TAB )
+             .sendKeys( "+905363277775" )
+             .sendKeys( Keys.TAB )
+             .sendKeys( Keys.TAB )
+             .sendKeys( "Ocak" )
+             .sendKeys( Keys.TAB )
+             .sendKeys( "11" )
+             .sendKeys( Keys.TAB )
+             .sendKeys( "1972" )
+             .sendKeys( Keys.TAB )
+             .perform();
+     driver.findElement( By.xpath( "//*[text()='İleri']" ) ).click();
+     }
 
 }
